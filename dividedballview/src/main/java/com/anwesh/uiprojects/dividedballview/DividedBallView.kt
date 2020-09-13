96291d16a0f58bf6ae477522a8619978a6e8fec7
Anwesh43 <anweshthecool0@gmail.com>
2020-09-13 14:04:18 +0530

    adding DBNode class

--- dividedballview/src/main/java/com/anwesh/uiprojects/dividedballview/DividedBallView.kt
+++ dividedballview/src/main/java/com/anwesh/uiprojects/dividedballview/DividedBallView.kt
@@ -119,4 +119,45 @@ class DividedBallView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class DBNode(var i : Int, val state : State = State()) {
+
+        private var next : DBNode? = null
+        private var prev : DBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = DBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawDBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : DBNode {
+            var curr : DBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
