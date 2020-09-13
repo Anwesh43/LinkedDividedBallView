68433ab8c4a198a26cb3205fe0bef975f40773fa
Anwesh43 <anweshthecool0@gmail.com>
2020-09-13 14:05:58 +0530

    adding DividedBall class

--- dividedballview/src/main/java/com/anwesh/uiprojects/dividedballview/DividedBallView.kt
+++ dividedballview/src/main/java/com/anwesh/uiprojects/dividedballview/DividedBallView.kt
@@ -160,4 +160,27 @@ class DividedBallView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class DividedBall(var i : Int) {
+
+        private var curr : DBNode = DBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            curr.update {
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
