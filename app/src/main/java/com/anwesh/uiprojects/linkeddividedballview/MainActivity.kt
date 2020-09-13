7d3226b7739eba90d32c5ba9bb0a1a6cd5a47d2f
Anwesh43 <anweshthecool0@gmail.com>
2020-09-13 14:45:01 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkeddividedballview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkeddividedballview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkeddividedballview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.dividedballview.DividedBallView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DividedBallView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
