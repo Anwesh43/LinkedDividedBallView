b288e4d9e807a02be5345d256609294c40716b59
Anwesh43 <anweshthecool0@gmail.com>
2020-09-13 13:00:56 +0530

    adding constants and libraries

--- dividedballview/src/main/java/com/anwesh/uiprojects/dividedballview/DividedBallView.kt
+++ dividedballview/src/main/java/com/anwesh/uiprojects/dividedballview/DividedBallView.kt
@@ -3,3 +3,24 @@ package com.anwesh.uiprojects.dividedballview
 /**
  * Created by anweshmishra on 13/09/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val colors : Array<Int> = arrayOf(
+        "#F44336",
+        "#4CAF50",
+        "#FF5722",
+        "#3F51B5",
+        "#2196F3"
+).map({Color.parseColor(it)}).toTypedArray()
+val parts : Int = 3
+val scGap : Float = 0.02f / parts
+val sizeFactor : Float = 14.6f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
