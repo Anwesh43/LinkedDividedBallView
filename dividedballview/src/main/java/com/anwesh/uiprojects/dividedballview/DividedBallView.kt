1a095dd0c76ea916c0ddc69d378040b9f0429990
Anwesh43 <anweshthecool0@gmail.com>
2020-09-13 13:22:06 +0530

    adding Canvas extension method

--- dividedballview/src/main/java/com/anwesh/uiprojects/dividedballview/DividedBallView.kt
+++ dividedballview/src/main/java/com/anwesh/uiprojects/dividedballview/DividedBallView.kt
@@ -29,3 +29,30 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n .inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawDividedBall(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sc1 : Float = scale.divideScale(0, parts)
+    val sc2 : Float = scale.divideScale(1, parts)
+    val sc3 : Float = scale.divideScale(2, parts)
+    val sf2 : Float = sc2.sinify()
+    val r : Float = Math.min(w, h) / (2 * sizeFactor)
+    val x : Float = (w / 2 - r) * sf2
+    val y : Float = (h / 2 - r) * (1 - 2 * sc2)
+    val currR : Float = r * (sc1 - sc3)
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawCircle(x, y, currR, paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawDBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    drawDividedBall(scale, w, h, paint)
+}
